733e64a9099dfabd9b3a6f43a30fb201453116a8
hxz <948516010@qq.com>
2023-10-08 15:29:24 +0800

    char dev Tested

--- onic_main.c
+++ onic_main.c
@@ -28,6 +28,9 @@
 #include "onic_lib.h"
 #include "onic_common.h"
 #include "onic_netdev.h"
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/uaccess.h>
 
 #ifndef ONIC_VF
 #define DRV_STR "OpenNIC Linux Kernel Driver"
@@ -36,7 +39,7 @@ char onic_drv_name[] = "onic";
 #define DRV_STR "OpenNIC Linux Kernel Driver (VF)"
 char onic_drv_name[] = "open-nic-vf";
 #endif
-
+#define DEVICE_NAME "spmv_dev"
 #define DRV_VER "0.21"
 const char onic_drv_str[] = DRV_STR;
 const char onic_drv_ver[] = DRV_VER;
@@ -48,7 +51,10 @@ MODULE_VERSION(DRV_VER);
 
 static int RS_FEC_ENABLED=1;
 module_param(RS_FEC_ENABLED, int, 0644);
-
+struct class *class; 
+struct device *device; 
+static dev_t spmv_dev;
+static struct cdev spmv_cdev;
 
 static const struct pci_device_id onic_pci_tbl[] = {
 	/* PCIe lane width x16 */
@@ -69,14 +75,34 @@ static const unsigned char onic_default_dev_addr[] = {
 	0x00, 0x0A, 0x35, 0x00, 0x00, 0x00
 };
 
-static const struct net_device_ops onic_netdev_ops = {
-	.ndo_open = onic_open_netdev,
-	.ndo_stop = onic_stop_netdev,
-	.ndo_start_xmit = onic_xmit_frame,
-	.ndo_set_mac_address = onic_set_mac_address,
-	.ndo_do_ioctl = onic_do_ioctl,
-	.ndo_change_mtu = onic_change_mtu,
-	.ndo_get_stats64 = onic_get_stats64,
+// static const struct net_device_ops onic_netdev_ops = {
+// 	.ndo_open = onic_open_netdev,
+// 	.ndo_stop = onic_stop_netdev,
+// 	.ndo_start_xmit = onic_xmit_frame,
+// 	.ndo_set_mac_address = onic_set_mac_address,
+// 	.ndo_do_ioctl = onic_do_ioctl,
+// 	.ndo_change_mtu = onic_change_mtu,
+// 	.ndo_get_stats64 = onic_get_stats64,
+// };
+
+static int mychardev_open(struct inode *inode, struct file *file)
+{
+    printk(KERN_INFO "spmvdev: Device opened\n");
+    return 0;
+}
+
+static int mychardev_release(struct inode *inode, struct file *file)
+{
+    printk(KERN_INFO "spmvdev: Device closed\n");
+    return 0;
+}
+
+static struct file_operations spmv_fops = {
+    .owner = THIS_MODULE,
+    .open = mychardev_open,
+    .release = mychardev_release,
+    // .read = mychardev_read,
+    // .write = onic_xmit_frame,
 };
 
 extern void onic_set_ethtool_ops(struct net_device *netdev);
@@ -172,6 +198,9 @@ static int onic_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 
 	pci_set_drvdata(pdev, priv);
 
+
+    device = device_create(class,NULL,spmv_dev,NULL,DEVICE_NAME);
+
 	dev_info(&pdev->dev,"Setup OK!");
 	return 0;
 
@@ -206,12 +235,12 @@ static void onic_remove(struct pci_dev *pdev)
 
 	pci_set_drvdata(pdev, NULL);
 	pci_release_mem_regions(pdev);
-
+    
 	dev_info(&pdev->dev,"Rm OK!");
 
 	pci_disable_device(pdev);
 	kfree(priv);
-	
+	device_destroy(class,spmv_dev);  //注销设备
 }
 
 /* static const struct pci_error_handlers qdma_err_handler = { */
@@ -235,13 +264,46 @@ static struct pci_driver pci_driver = {
 
 static int __init onic_init_module(void)
 {
+    int major_num,minor_num;
 	pr_info("%s %s", onic_drv_str, onic_drv_ver);
+	
+	if (alloc_chrdev_region(&spmv_dev, 0, 1, DEVICE_NAME) < 0) {
+        printk(KERN_ERR "Failed to allocate character device region\n");
+        return -1;
+    }
+
+	cdev_init(&spmv_cdev, &spmv_fops);
+	
+	if (cdev_add(&spmv_cdev, spmv_dev, 1) < 0) {
+        printk(KERN_ERR "Failed to add character device\n");
+        unregister_chrdev_region(spmv_dev, 1);
+        return -1;
+    }
+    major_num = MAJOR(spmv_dev);
+    minor_num = MINOR(spmv_dev);
+    printk(KERN_INFO"spmvdev: device  major_num = %d\n",major_num);
+    printk(KERN_INFO"spmvdev: device minor_num = %d\n",minor_num);
+
+
+    class = class_create(THIS_MODULE,DEVICE_NAME); //注册类
+	// 
+
+
+    printk(KERN_INFO "spmvdev: Character device registered\n");
+  
+	// return 0;
 	return pci_register_driver(&pci_driver);
 }
 
 static void __exit onic_exit_module(void)
 {
+	
 	pci_unregister_driver(&pci_driver);
+
+	cdev_del(&spmv_cdev);
+    unregister_chrdev_region(spmv_dev, 1);
+	class_destroy(class); //注销类
+    printk(KERN_INFO "spmvdev: Character device unregistered\n");
 }
 
 module_init(onic_init_module);
